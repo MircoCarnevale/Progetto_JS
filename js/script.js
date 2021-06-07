d75158fa88f7b1cdf792cb2c503098556caec8dd
Mirco_Carnevale <mirco.carnevale23@gmail.com>
2021-06-07 09:22:58 +0200

    final commit

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 
 let xhr = new XMLHttpRequest();
-//console.log('Cambio stato: ' + xhr.readyState);
 xhr.open('GET', 'https://jsonplaceholder.typicode.com/users');
-//console.log('Cambio stato: ' + xhr.readyState);
 xhr.send();
 xhr.onreadystatechange = function() {
-   // console.log('Cambio stato: ' + xhr.readyState);
-   // console.log(xhr.responseText);
    if(xhr.readyState === 4 && xhr.status === 200) {
        let Obj = JSON.parse(xhr.responseText);
        console.log(Obj);
@@ -14,8 +10,11 @@ xhr.onreadystatechange = function() {
        let i=0;
        for (const user of Obj) {
            console.log(user.name);
-           console.log(body.children[1].children[0].children[1].children[i].children[1].innerHTML);
-           body.children[1].children[0].children[1].children[i].children[1].innerHTML = user.nome;
+           console.log(user.username);
+           body.children[1].children[0].children[1].children[i].children[1].innerHTML = user.name;
+           body.children[1].children[0].children[1].children[i].children[2].innerHTML = user.username;
+           body.children[1].children[0].children[1].children[i].children[3].innerHTML = user.email;
+           body.children[1].children[0].children[1].children[i].children[4].innerHTML = user.phone;
            i++;
        }
    }
